6ff7807a604cc2a62eef493ad3d0e87687f42a95
Hannaamal <hannaamal2715@gmail.com>
2026-01-13 09:59:46 +0530

    test

--- server.js
+++ server.js
@@ -68,9 +68,12 @@ app.use(
     },
     credentials: true,
     methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
-    allowedHeaders: ["Content-Type", "Authorization"],
+    allowedHeaders: ["Content-Type", "Authorization", "Cookie"],
+
   })
 );
+app.use(cors());
+
 
 
 
