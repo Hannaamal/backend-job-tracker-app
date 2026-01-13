6a66eb78dade50ff25f39b7ce5d38f12a9379f70
Hannaamal <hannaamal2715@gmail.com>
2026-01-13 12:49:10 +0530

    test

--- server.js
+++ server.js
@@ -54,21 +54,13 @@ const allowedOrigins = [
 
 app.use(
   cors({
-    origin: (origin, callback) => {
-      if (!origin) return callback(null, true); // allow Postman or curl
-
-      // allow exact matches
-      if (allowedOrigins.includes(origin)) return callback(null, true);
-
-      // allow any vercel preview deploy
-      if (/\.vercel\.app$/.test(origin)) return callback(null, true);
-
-      console.warn("Blocked by CORS:", origin);
-      return callback(null, false); // reject gracefully
-    },
+    origin: [
+      "http://localhost:3000",
+      "https://job-portal-frontend-2u0pb7f1b-amalhannas-projects.vercel.app/"
+    ],
     credentials: true,
     methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
-    allowedHeaders: ["Content-Type", "Authorization", "Cookie"],
+    allowedHeaders: ["Content-Type", "Authorization"],
   })
 );
 
